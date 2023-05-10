99de6183cc7f33584763fdc1d8e2ec5a932e404d
kristianjackson <kristian.jackson@gmail.com>
2023-05-10 14:57:10 -0400

    still working on search

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,32 @@ from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper, SerpAPIWrapper
 from langchain.agents import load_tools, initialize_agent, AgentType
 
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def scrape_webpage(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Remove all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract() 
+    
+    # Get text
+    text = soup.get_text()
+    
+    # Remove leading and trailing spaces
+    lines = (line.strip() for line in text.splitlines())
+    
+    # Break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    
+    # Drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    
+    return text
+
 # App framework
 st.title("🦜🔗 White Paper Generator")
 prompt = st.text_input("What topic would you like the white paper to be about?")
@@ -49,14 +75,16 @@ creative_llm = OpenAI(temperature=0.4)
 
 # Chains
 title_chain = LLMChain(
-    llm=creative_llm,
+    # llm=creative_llm,
+    llm=llm,
     prompt=title_template,
     verbose=True,
     output_key="title",
     memory=title_memory,
 )
 exec_summary_chain = LLMChain(
-    llm=creative_llm,
+    # llm=creative_llm,
+    llm=llm
     prompt=exec_summary_template,
     verbose=True,
     output_key="exec_summary",
@@ -87,6 +115,9 @@ if prompt:
         f"Find content related to {prompt} for use in generating a white paper based on current information no older than 1 year from today's date."
     )
     st.write(search_results)
+    
+    scraped_content = [scrape_webpage(url) for url in search_results]
+    st.write(scraped_content)
 
     title = title_chain.run(topic=prompt, search_results=search_results)
     st.write(title)
